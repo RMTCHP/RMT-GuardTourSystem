1bb19ca9b2eab8b38c87ad72556c56af2be83080
RMTCHP <rmtchp@gmail.com>
2026-07-07 08:31:33 +0700

    Update admin.management.js

--- admin.management.js
+++ admin.management.js
@@ -103,6 +103,7 @@ function invalidateAdminCaches() {
   state.dashboardSnapshotCache = {};
   state.dashboardChartsCache = {};
 }
+
 async function openAddUserSwal(existingGuard) {
   if (!state.supervisor) return;
   const isEdit = !!existingGuard;
